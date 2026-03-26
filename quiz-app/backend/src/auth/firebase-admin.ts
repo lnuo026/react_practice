ff6936d7ac65e059fff93a0bf4db89a0fc373445
Liang Nuona <lnuo026@aucklanduni.ac.nz>
2026-03-27 00:33:16 +1300

    test

--- quiz-app/backend/src/auth/firebase-admin.ts
+++ quiz-app/backend/src/auth/firebase-admin.ts
@@ -2,10 +2,9 @@ import * as admin from 'firebase-admin';
 // admin.apps 是已初始化的 app列表。
 // 这个判断防止重复初始化（被多次 import 时只初始化一次）
 if(!admin.apps.length){
+    const serviceAccount = require('../../quizapp-guuuuda-firebase-adminsdk-fbsvc-e756c3ba21.json')
     admin.initializeApp({
-        // 用服务器默认凭证认证（本地开发环境需要设置   
-        credential:admin.credential.applicationDefault(),
-        projectId:process.env.FIREBASE_PROJECT_ID, 
+        credential: admin.credential.cert(serviceAccount),
     })
 }
 
